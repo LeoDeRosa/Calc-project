725e51ccfc5bb1b0db2a2fd9581cbd88d2542db3
lol DeRosa <leonardord04@gmail.com>
2022-05-24 20:42:58 -0700

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,9 +1,22 @@
 
 function beeaans() {
     var Beanss = String;
+    
     Beanss = document.getElementById('input');
     document.getElementById('beans').innerText = (Beanss.value);
-    const Beansssize = document.getElementById('beans').length;
-    document.getElementById('Beans').innerText = Beansssize;
-    console.log(Beansssize);
+    const Beansssize = Beanss.value.length;
+    document.getElementById('Beans').innerText = checkInput(Beanss);
+    
+}
+
+function checkInput(input){
+    const badInputs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    for (i in Range(input)) {
+        if (badInputs.includes(i)){
+            return("Bad Input")
+        }
+    }
+    return("Good Input")
+
 }
