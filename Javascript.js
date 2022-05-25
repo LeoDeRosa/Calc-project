fde438d0d938ebbd3a2ab736643587c73c0a9936
Jacob Hepburn <JacobHepburn04@gmail.com>
2022-05-24 19:52:16 -0700

    stuff was done

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,9 @@
-let btn = document.getElementsByClassName(button);
 
-function input() {
-    //stuff
+function beeaans() {
+    var Beanss = String;
+    Beanss = document.getElementById('input');
+    document.getElementById('beans').innerText = (Beanss.value);
+    let Beansssize = Beanss.length;
+    document.getElementById('Beans').innerText = (Beansssize);
+    console.log(Beansssize);
 }
-
-
-
-
-
-
-
-
-
