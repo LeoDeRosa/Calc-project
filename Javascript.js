bf05286209520f5e75db56884e36e032d8bc1c6a
Jacob Hepburn <JacobHepburn04@gmail.com>
2022-05-29 14:18:16 -0700

    Did work
    More or less finished css and Html.
    Worked on Javascript a little too. Didn't bother working on the checking for an invalid entry though

--- Javascript.js
+++ Javascript.js
@@ -1,22 +1,55 @@
 
 function beeaans() {
+
     var Beanss = String;
-    
     Beanss = document.getElementById('input');
-    document.getElementById('beans').innerText = (Beanss.value);
-    const Beansssize = Beanss.value.length;
-    document.getElementById('Beans').innerText = checkInput(Beanss);
+    Beanss = Beanss.value;
+
+    if (Beanss.value != "")
+    {
+        document.getElementById("beans1").innerText = "F(x)=";
+        document.getElementById('beans2').innerText = (Beanss);
+    }
+
+    //if (checkInput(Beanss))
+    //{
+        document.getElementById("beans3").innerText = "F'(x)=";
+        var Beanssses = String;
+        Beanssses = derivitive(Beanss);
+        document.getElementById("beans4").innerText = (Beanssses);
+    //}
+    //else
+        //document.getElementById("Invalid Fuction, Please check Initial Fuction")
+
     
 }
-
 function checkInput(input){
-    const badInputs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    const badInputs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"];
 
     for (i in Range(input)) {
         if (badInputs.includes(i)){
-            return("Bad Input")
+            return(false)
         }
     }
-    return("Good Input")
+    return(true)
 
+}
+function derivitive(input)
+{
+    var a = String;
+    var b = String;
+    a = input;
+
+    //addition rule
+    if ((a.includes("-")) || (a.includes("+"))) {
+        if ((a.includes("(")) || a.includes(")")){
+            
+        }
+        else{
+            
+        }
+    }
+    else{
+        return "false";
+    }
 }
